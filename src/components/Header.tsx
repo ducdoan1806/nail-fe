b88858665624295dc0b8de0c672568ea2c61de92
ducdoan1806 <ducdoan1806@gmail.com>
2024-10-07 14:03:35 +0700

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
   const { cartItems } = useCart();
   const pathname = usePathname();
   return (
-    <header className="bg-white shadow-md sticky top-0">
+    <header className="bg-white shadow-md sticky top-0 z-10">
       <div className="container mx-auto px-5 py-3 flex items-center justify-between">
         <div className="flex items-center">
           <i className="fas fa-bars text-xl md:hidden"></i>
