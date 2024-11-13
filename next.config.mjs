652f66ed334ec7df0b58eb35bf15dbecc24d8a0b
ducdoan1806 <ducdoan1806@gmail.com>
2024-11-13 13:22:05 +0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: { domains: ["172.18.0.3",'localhost'] },
+  images: { domains: ["172.18.0.3", "localhost", "127.0.0.1"] },
 };
 
 export default nextConfig;
